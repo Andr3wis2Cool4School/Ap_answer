fba94fcda59a26af81f0b91db77d40c8ed7dd1b6
Andrew <20448589@life.hkbu.edu.hk>
2021-06-21 08:52:11 +0800

    a

---
+++ ex/loopArray2.java
@@ -0,0 +1,9 @@
+public class loopArray2{
+    public static void main(String[] args){
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i: cars){
+            System.out.println(i);
+        }
+    }
+}
+
